38547612f4d5d32de8bdb9c2b3a1635bb6a3559a
Kangmin <mk@kangmin.kr>
2023-07-30 22:17:07 +0900

    first commit

--- prisma/data.js
+++ prisma/data.js
@@ -1,4 +1,4 @@
-const { Prisma } = require('@prisma/client');
+const { Prisma } = require( '@prisma/client');
 
 const categories = [
   {
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,20 @@
-const { PrismaClient } = require('@prisma/client');
-const { categories, products } = require('./data.js');
+const { PrismaClient } = require( '@prisma/client');
+const { categories, products } = require( './data.js');
 const prisma = new PrismaClient();
 
 const load = async () => {
   try {
-    await prisma.category.deleteMany();
-    console.log('Deleted records in category table');
+    // await prisma.category.deleteMany();
+    // console.log('Deleted records in category table');
 
-    await prisma.product.deleteMany();
-    console.log('Deleted records in product table');
+    // await prisma.product.deleteMany();
+    // console.log('Deleted records in product table');
 
-    await prisma.$queryRaw`ALTER TABLE Product AUTO_INCREMENT = 1`;
-    console.log('reset product auto increment to 1');
+    // await prisma.$queryRaw`ALTER TABLE Product AUTO_INCREMENT = 1`;
+    // console.log('reset product auto increment to 1');
 
-    await prisma.$queryRaw`ALTER TABLE Category AUTO_INCREMENT = 1`;
-    console.log('reset category auto increment to 1');
+    // await prisma.$queryRaw`ALTER TABLE Category AUTO_INCREMENT = 1`;
+    // console.log('reset category auto increment to 1');
 
     await prisma.category.createMany({
       data: categories,
@@ -25,6 +25,7 @@ const load = async () => {
       data: products,
     });
     console.log('Added product data');
+
   } catch (e) {
     console.error(e);
     process.exit(1);
